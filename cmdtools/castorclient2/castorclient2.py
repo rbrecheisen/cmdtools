e5b54a82ff0f04a5d8ce3a91a9828bc96b6ae5a3
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2020-11-06 17:07:11 +0100

    Saving work

--- cmdtools/castorclient2/castorclient2.py
+++ cmdtools/castorclient2/castorclient2.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
@@ -15,6 +18,68 @@ using a wrapper shell script based on the cmd2 library.
 BASE_URL = 'https://data.castoredc.com'
 TOKEN_URL = BASE_URL + '/oauth/token'
 API_URL = BASE_URL + '/api'
+
+
+class ProcessingStep(object):
+
+    def execute(self, df):
+        raise NotImplementedError('Implement this method')
+
+
+class ConvertColumnFromIntToString(ProcessingStep):
+
+    def __init__(self, column_name):
+        self.column_name = column_name
+
+    def execute(self, df):
+        new_values = []
+        for v in df[self.column_name].values:
+            if not np.isnan(v):
+                new_values.append(str(int(v)))
+            else:
+                new_values.append(None)
+        df[self.column_name] = pd.Series(data=new_values, dtype=object)
+        return df
+
+
+class ChangeColumnDatesToFormattedString(ProcessingStep):
+
+    def __init__(self, date_format):
+        self.date_format = date_format
+
+    def execute(self, df):
+        for column in df.columns:
+            if df[column].dtype == 'datetime64[ns]':
+                df[column] = df[column].dt.strftime(self.date_format)
+        return df
+
+
+class MapDicaToCastorColumnNames(ProcessingStep):
+
+    def __init__(self, mapping_file, k, v):
+        self.mapping_json = self.load_mapping_to_json(mapping_file, k, v)
+
+    @staticmethod
+    def load_mapping_to_json(mapping_file, k, v):
+        mapping = pd.read_excel(mapping_file)
+        mapping_json = {}
+        for _, row in mapping.iterrows():
+            if isinstance(row[v], float):
+                mapping_json[row[k]] = None
+            else:
+                mapping_json[row[k]] = row[v]
+        return mapping_json
+
+    def execute(self, df):
+        new_columns = []
+        for column in df.columns:
+            new_column = self.mapping_json[column]
+            if new_column is not None:
+                new_columns.append(new_column)
+            else:
+                new_columns.append(column)
+        df.columns = new_columns
+        return df
 
 
 class CastorExportClient(object):
@@ -62,6 +127,17 @@ class CastorImportAndUpdateClient(object):
             studies.append(study)
             idx += 1
         return studies
+
+    @staticmethod
+    def import_dica_data(file_path):
+        df = pd.read_excel(file_path)
+        processing_steps = [
+            ConvertColumnFromIntToString(column_name='upn'),
+            ChangeColumnDatesToFormattedString(date_format='%d-%m-%Y'),
+            MapDicaToCastorColumnNames(mapping_file='', k='dica', v='castor')
+        ]
+        for processing_step in processing_steps:
+            df = processing_step.execute(df)
 
 
 class CastorClient(CastorExportClient, CastorImportAndUpdateClient):
--- cmdtools/castorclient2/test/test_castorclient2.py
+++ cmdtools/castorclient2/test/test_castorclient2.py
@@ -9,31 +9,36 @@ with open('{}/castorclientsecret.txt'.format(os.environ['HOME']), 'r') as f:
     CLIENT_SECRET = f.readline().strip()
 
 
-STUDY_DPCA = 'ESPRESSO_v2.0_DPCA'
-STUDY_DHBA = 'ESPRESSO_v2.0_DHBA'
+DPCA_STUDY = 'ESPRESSO_v2.0_DPCA'
+DHBA_STUDY = 'ESPRESSO_v2.0_DHBA'
+DHBA_EXCEL_FILE_PATH = '/Users/ralph/Desktop/Export_DHBA_academisch-ziekenhuis-maastricht_01042020.xlsx'
 
 
 class TestCastorClient(unittest.TestCase):
 
     def test_client(self):
 
-        # Connect to Castor EDC and verify that session is valid
         self.client = CastorClient()
-        self.client.connect(CLIENT_ID, CLIENT_SECRET)
-        self.assertIsNotNone(self.client._session)
 
-        # Verify that selected study is None
-        self.assertIsNone(self.client._selected_study)
+        # # Connect to Castor EDC and verify that session is valid
+        # self.client.connect(CLIENT_ID, CLIENT_SECRET)
+        # self.assertIsNotNone(self.client._session)
+        #
+        # # Verify that selected study is None
+        # self.assertIsNone(self.client._selected_study)
+        #
+        # # Select DPCA study
+        # self.client.select_study(DPCA_STUDY)
+        # self.assertIsNotNone(self.client._selected_study)
+        # self.assertEqual(self.client._selected_study['name'], DPCA_STUDY)
+        #
+        # # Select DHBA study
+        # self.client.select_study(DHBA_STUDY)
+        # self.assertIsNotNone(self.client._selected_study)
+        # self.assertEqual(self.client._selected_study['name'], DHBA_STUDY)
 
-        # Select DPCA study
-        self.client.select_study(STUDY_DPCA)
-        self.assertIsNotNone(self.client._selected_study)
-        self.assertEqual(self.client._selected_study['name'], STUDY_DPCA)
-
-        # Select DHBA study
-        self.client.select_study(STUDY_DHBA)
-        self.assertIsNotNone(self.client._selected_study)
-        self.assertEqual(self.client._selected_study['name'], STUDY_DHBA)
+        # Import DICA data
+        self.client.import_dica_data(DHBA_EXCEL_FILE_PATH)
 
 
 if __name__ == '__main__':
