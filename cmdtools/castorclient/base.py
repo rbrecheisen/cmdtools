2328b756e990dc7699a3c77c66c7845a7b384b58
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2020-11-11 21:01:15 +0100

    Working on Castor client

--- cmdtools/castorclient/base.py
+++ cmdtools/castorclient/base.py
@@ -1,21 +1,42 @@
+import pandas as pd
+
+
 class CastorClient(object):
 
-    def import_dhba_data(self, excel_file_path):
+    def __init__(self):
+
+        self.df = None
+        self.dd = None
+        self.op = None
+
+    def load_export_excel(self, excel_file):
+
+        self.df = pd.read_excel(excel_file, sheet_name='Study results')
+        self.df.columns = self.df.columns.str.replace(' ', '_')
+        self.dd = pd.read_excel(excel_file, sheet_name='Study variable list')
+        self.dd.columns = self.dd.columns.str.replace(' ', '_')
+        self.op = pd.read_excel(excel_file, sheet_name='Field options')
+        self.op.columns = self.op.columns.str.replace(' ', '_')
+
+        self.update_column_data_types()
+
+    def update_column_data_types(self):
         pass
 
-    def test_drive_upload_to_cloud_instance(self, data, instance):
+    def convert_columns_int_to_str(self):
         pass
 
-    def get_upload_test_report(self):
-        test_report = UploadTestReport()
-        return test_report
+    def convert_date_columns_to_str(self, columns):
+        for column in columns:
+            if self.df[column].dtype == 'datetime64[ns]':
+                self.df[column] = df[column].dt.strftime('%d-%m-%Y')
 
-
-class CastorCloudInstance(object):
-    pass
-
-
-class UploadTestReport(object):
-
-    def is_test_upload_successful(self):
-        return True
+    def is_unique_key(self, key_columns):
+        """
+        Checks whether records can be uniquely identified using key_columns. For example,
+        Using a SAP number and surgery date you should be able to uniquely identify each
+        record in the currently selected Castor study.
+        :param key_columns: Column combination that unique identifies a record.
+        :return: True/False
+        """
+        pass
--- cmdtools/castorclient/test/test_acceptance.py
+++ cmdtools/castorclient/test/test_acceptance.py
@@ -3,42 +3,17 @@ import os
 from cmdtools.castorclient import CastorClient, CastorCloudInstance
 
 
-# TODO: Move these globals to a separate file
-HOME = os.environ['HOME']
-with open('{}/castorclientid.txt'.format(HOME), 'r') as f:
-    CLIENT_ID = f.readline().strip()
-with open('{}/castorclientsecret.txt'.format(HOME), 'r') as f:
-    CLIENT_SECRET = f.readline().strip()
-DPCA_STUDY = 'ESPRESSO_v2.0_DPCA'
-DHBA_STUDY = 'ESPRESSO_v2.0_DHBA'
-DHBA_EXCEL_FILE_PATH = '{}/Desktop/Export_DHBA_academisch-ziekenhuis-maastricht_01042020.xlsx'.format(HOME)
-DHBA_COLUMNS_INT_TO_STRING = ['upn', 'verrichting-upn']
-DHBA_MAPPING_FILE_PATH = '{}/Desktop/DHBA_var_mapping_to_Castor.xlsx'.format(HOME)
-DHBA_KEY_COLUMN = 'dica'
-DHBA_VALUE_COLUMN = 'castor'
+# # TODO: Move these globals to a separate file
+# HOME = os.environ['HOME']
+# with open('{}/castorclientid.txt'.format(HOME), 'r') as f:
+#     CLIENT_ID = f.readline().strip()
+# with open('{}/castorclientsecret.txt'.format(HOME), 'r') as f:
+#     CLIENT_SECRET = f.readline().strip()
+# DPCA_STUDY = 'ESPRESSO_v2.0_DPCA'
+# DHBA_STUDY = 'ESPRESSO_v2.0_DHBA'
+# DHBA_EXCEL_FILE_PATH = '{}/Desktop/Export_DHBA_academisch-ziekenhuis-maastricht_01042020.xlsx'.format(HOME)
+# DHBA_COLUMNS_INT_TO_STRING = ['upn', 'verrichting-upn']
+# DHBA_MAPPING_FILE_PATH = '{}/Desktop/DHBA_var_mapping_to_Castor.xlsx'.format(HOME)
+# DHBA_KEY_COLUMN = 'dica'
+# DHBA_VALUE_COLUMN = 'castor'
 
-
-def test_drive_upload_dhba_data_to_castor_cloud_instance():
-    """
-    Tests whether the test drive of uploading DHBA data to Castor EDC works as expected, i.e., there is
-    no actual data uploaded but a report is created that specifies exactly which new records will be
-    created, as well as which records will be skipped (because they already exist in Castor).
-    """
-
-    # Create Castor client
-    client = CastorClient()
-
-    # Import DHBA data into client
-    data = client.import_dhba_data(DHBA_EXCEL_FILE_PATH)
-
-    # Connect to Castor EDC cloud
-    cloud_instance = CastorCloudInstance()
-
-    # Test drive the upload of DHBA data to Castor EDC cloud
-    client.test_drive_upload_to_cloud_instance(data, cloud_instance)
-
-    # Get test report for the upload test drive
-    test_report = client.get_upload_test_report()
-
-    # Verify the test report indicates a successful test drive
-    assert test_report.is_test_upload_successful()
